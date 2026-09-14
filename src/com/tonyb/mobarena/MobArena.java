1ed9a37873a9aff09274ccf9f4fe1410471d53ba
Tony Billot <100514277+Tony-Billot@users.noreply.github.com>
2026-09-14 20:24:39 +0200

    chore: initial project setup

---
+++ src/com/tonyb/mobarena/MobArena.java
@@ -0,0 +1,19 @@
+package com.tonyb.mobarena;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MobArena extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		System.out.println("Started MobArena");
+		
+		super.onEnable();
+	}
+	
+	@Override
+	public void onDisable() {
+		super.onDisable();
+	}
+
+}
