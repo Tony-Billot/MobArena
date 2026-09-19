236318d3b9a1803d262a4106a297df9d82c007a3
Tony Billot <100514277+Tony-Billot@users.noreply.github.com>
2026-09-19 12:47:31 +0200

    fix: allow mobs to burn, except in the sun

--- src/com/tonyb/mobarena/events/MobEvents.java
+++ src/com/tonyb/mobarena/events/MobEvents.java
@@ -1,13 +1,28 @@
 package com.tonyb.mobarena.events;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Skeleton;
+import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityCombustByBlockEvent;
+import org.bukkit.event.entity.EntityCombustByEntityEvent;
 import org.bukkit.event.entity.EntityCombustEvent;
 
 public class MobEvents implements Listener {
 	
 	@EventHandler
 	public void onEntityCombust(EntityCombustEvent event) {
+	    Entity entity = event.getEntity();
+
+	    if (!(entity instanceof Zombie) && !(entity instanceof Skeleton)) {
+	        return;
+	    }
+
+	    if (event instanceof EntityCombustByBlockEvent || event instanceof EntityCombustByEntityEvent) {
+	        return;
+	    }
+
 	    event.setCancelled(true);
 	}
 
